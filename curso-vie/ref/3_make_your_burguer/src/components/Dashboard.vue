753b481ec4ce7a725e3f7d89c12b0d47e3a2e244
Leonardo Simas <leonardosimasrodrigues@gmail.com>
2024-05-20 10:24:35 -0300

    Projeto Make your Burguer

---
+++ curso-vie/ref/3_make_your_burguer/src/components/Dashboard.vue
@@ -0,0 +1,190 @@
+<template>
+  <div id="burguer-table">
+    <Message :msg="msg" v-show="msg" />
+    <div>
+      <div id="burguer-table-heading">
+        <div class="order-id">#:</div>
+        <div>Cliente:</div>
+        <div>Pão:</div>
+        <div>Carne:</div>
+        <div>Opcionais:</div>
+        <div>Ações:</div>
+      </div>
+      <div id="burguer-table-rows">
+        <div
+          class="burguer-table-row"
+          v-for="burguer in burguers"
+          :key="burguer.id"
+        >
+          <div class="order-number">{{ burguer.id }}</div>
+          <div>{{ burguer.nome }}</div>
+          <div>{{ burguer.pao }}</div>
+          <div>{{ burguer.carne }}</div>
+          <div>
+            <ul>
+              <li v-for="(opcional, index) in burguer.opcionais" :key="index">
+                {{ opcional }}
+              </li>
+            </ul>
+          </div>
+          <div>
+            <select
+              name="status"
+              class="status"
+              @change="updatedBurguer($event, burguer.id)"
+            >
+              <option value="" disabled selected>Selecione</option>
+              <option
+                v-for="s in status"
+                :key="s.id"
+                :value="s.tipo"
+                :selected="burguer.status == s.tipo"
+              >
+                {{ s.tipo }}
+              </option>
+            </select>
+
+            <button class="delete-btn" @click="deleteBurguer(burguer.id)">
+              Cancelar
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Message from "./Message.vue";
+
+export default {
+  name: "Dashboard",
+  data() {
+    return {
+      burguers: null,
+      burguer_id: null,
+      status: [],
+      msg: null,
+    };
+  },
+  components: {
+    Message,
+  },
+  methods: {
+    async getPedidos() {
+      const req = await fetch("http://localhost:3000/burgers");
+
+      const data = await req.json();
+
+      this.burguers = data;
+
+      // Resgatar os status
+      this.getStatus();
+    },
+    async getStatus() {
+      const req = await fetch("http://localhost:3000/status");
+
+      const data = await req.json();
+
+      this.status = data;
+
+      console.log(this.status);
+    },
+    async deleteBurguer(id) {
+      const req = await fetch(`http://localhost:3000/burgers/${id}`, {
+        method: "DELETE",
+      });
+
+      const res = await req.json();
+
+      //msg de pedido deletado
+      this.msg = "Pedido removido com sucesso!";
+
+      // limpar os campos
+      setTimeout(() => (this.msg = ""), 5000);
+
+      this.getPedidos();
+    },
+    async updatedBurguer(event, id) {
+      const option = event.target.value;
+
+      const dataJson = JSON.stringify({ status: option });
+
+      const req = await fetch(`http://localhost:3000/burgers/${id}`, {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        body: dataJson,
+      });
+
+      const res = await req.json();
+
+      //msg de pedido deletado
+      this.msg = `O pedido número ${res.id} foi atualizado para ${res.status}!`;
+
+      // limpar os campos
+      setTimeout(() => (this.msg = ""), 5000);
+
+      console.log(res);
+    },
+  },
+  mounted() {
+    this.getPedidos();
+  },
+};
+</script>
+
+<style scoped>
+#burguer-table {
+  max-width: 1200px;
+  margin: 0 auto;
+}
+#burguer-table-heading,
+#burguer-table-rows,
+.burguer-table-row {
+  display: flex;
+  flex-wrap: wrap;
+}
+#burguer-table-heading {
+  font-weight: bold;
+  padding: 12px;
+  border-bottom: 3px solid #333;
+}
+
+#burguer-table-heading div,
+.burguer-table-row div {
+  width: 19%;
+}
+
+.burguer-table-row {
+  width: 100%;
+  padding: 12px;
+  border-bottom: 1px solid #ccc;
+}
+
+#burguer-table-heading .order-id,
+.burguer-table-row .order-number {
+  width: 5%;
+}
+
+select {
+  padding: 12px 6px;
+  margin-right: 12px;
+}
+
+.delete-btn {
+  background-color: #222;
+  color: #fcba03;
+  font-weight: bold;
+  border: 2px solid #222;
+  padding: 10px;
+  font-size: 16px;
+  margin: 0 auto;
+  cursor: pointer;
+  transition: 0.5s;
+}
+
+.delete-btn:hover {
+  background-color: transparent;
+  color: #222;
+}
+</style>
---
+++ curso-vie/ref/3_make_your_burguer/src/components/Footer.vue
@@ -0,0 +1,42 @@
+<template>
+  <footer id="footer">
+    <p>Make your Burguer {{ dataAtual }} - &copy; {{ ano }}</p>
+  </footer>
+</template>
+  
+  <script>
+export default {
+  name: "Footer",
+  data() {
+    return {
+      dataAtual: null,
+      ano: null,
+    };
+  },
+  mounted() {
+    this.atualizarAno();
+  },
+  methods: {
+    atualizarAno() {
+      this.dataAtual = new Date().toLocaleDateString();
+
+      this.ano = new Date().getFullYear();
+    },
+  },
+};
+</script>
+  
+<style scoped>
+#footer {
+  height: 200px;
+  background-color: #222;
+  border-top: 4px solid #111;
+  color: #fcba03;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  bottom: 0;
+  width: 100%;
+}
+</style>
+  
